0020505c704a01ade71e9bc39991f728095a11f1
EEwing <eewing@cs.uoregon.edu>
2015-11-14 15:29:31 -0800

    Added basic app functions

---
+++ app/src/androidTest/java/rockr/elliottewing/com/rockr/ApplicationTest.java
@@ -0,0 +1,13 @@
+package rockr.elliottewing.com.rockr;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
